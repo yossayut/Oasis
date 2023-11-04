c24a343584618c0ad6dd4ff5681c0c1665ba1a2c
Yossayut Darakamas <yossayutdarakamas@gmail.com>
2023-11-05 03:00:26 +0700

    Add show key card function

--- App/LookupAndEdit/ShowKeycard.py
+++ App/LookupAndEdit/ShowKeycard.py
@@ -10,7 +10,7 @@ class Show_keycard_Page(tk.Toplevel):
 
     def __init__(self, master):
         super().__init__(master)
-        self.title("ห้องพักทั้งหมด")
+        self.title("บัตรคีย์การ์ดทั้งหมด")
         self.geometry("800x600")
         self.protocol("WM_DELETE_WINDOW", self.on_close)
         self.create_widgets()
@@ -22,22 +22,22 @@ class Show_keycard_Page(tk.Toplevel):
         filter_frame = ttk.Frame(paned_window)
         paned_window.add(filter_frame)
 
-        self.filter_var = tk.StringVar(value="ห้องทั้งหมด")
+        self.filter_var = tk.StringVar(value="บัตรคีย์การ์ดทั้งหมด")
 
         ttk.Radiobutton(filter_frame, 
-            text="ห้องทั้งหมด", 
+            text="บัตรคีย์การ์ดทั้งหมด", 
             value="Show all", 
             variable=self.filter_var, 
             command=lambda: self.change_filter("Show all")).grid(row=0, column=0, padx=10)
 
         ttk.Radiobutton(filter_frame, 
-            text="เฉพาะห้องที่ว่าง", 
-            value="Free rooms", 
+            text="บัตรคีย์การ์ดที่ว่าง", 
+            value="Free keycard", 
             variable=self.filter_var, 
-            command=lambda: self.change_filter("Free rooms")).grid(row=0, column=1, padx=10)
+            command=lambda: self.change_filter("Free keycard")).grid(row=0, column=1, padx=10)
 
         ttk.Radiobutton(filter_frame, 
-            text="เฉพาะห้องเช่าแล้ว", 
+            text="เฉพาะบัตรคีย์การ์ดที่ถูกใช้", 
             value="Rent rooms", 
             variable=self.filter_var, 
             command=lambda: self.change_filter("Rent rooms")).grid(row=0, column=2, padx=10)
@@ -49,17 +49,15 @@ class Show_keycard_Page(tk.Toplevel):
         	command=lambda: self.change_filter("Rent rooms order by customer")).grid(row=0, column=3, padx=10)
 
         self.tree = ttk.Treeview(paned_window, 
-        	columns=("Room ID", "Room Number", "Building", "Floor", "Room Type", "Rent", "Start Date", "End Date"), 
+        	columns=("Keycard No.", "Keycard ID", "Customer First Name Last Name", "Customer Room", "Staff First Name Last Name", "Status"), 
         	show="headings")
 
-        self.tree.heading("#1", text="ลำดับที่", command=lambda: self.sort_treeview(1, True))
-        self.tree.heading("#2", text="เบอร์ห้อง", command=lambda: self.sort_treeview(2, True))
-        self.tree.heading("#3", text="อาคาร", command=lambda: self.sort_treeview(3, True))
-        self.tree.heading("#4", text="ชั้น",    command=lambda: self.sort_treeview(4, True))
-        self.tree.heading("#5", text="ขนาดห้อง", command=lambda: self.sort_treeview(5, True))
-        self.tree.heading("#6", text="เช่า", command=lambda: self.sort_treeview(6, True))
-        self.tree.heading("#7", text="เริ่มต้น", command=lambda: self.sort_treeview(7, True))
-        self.tree.heading("#8", text="สิ้นสุด", command=lambda: self.sort_treeview(8, True))
+        self.tree.heading("#1", text="เลขที่บัตร", command=lambda: self.sort_treeview(1, True))
+        self.tree.heading("#2", text="ID บัตร", command=lambda: self.sort_treeview(2, True))
+        self.tree.heading("#3", text="ชื่อนามสกุลลูกค้า", command=lambda: self.sort_treeview(3, True))
+        self.tree.heading("#4", text="ห้อง", command=lambda: self.sort_treeview(4, True))
+        self.tree.heading("#5", text="ชื่อนามสกุลพนักงาน", command=lambda: self.sort_treeview(5, True))
+        self.tree.heading("#6", text="สถานะบัตร", command=lambda: self.sort_treeview(6, True))
 
         self.tree.column("#1", width=40,  anchor="center")
         self.tree.column("#2", width=80,  anchor="center")
@@ -67,64 +65,76 @@ class Show_keycard_Page(tk.Toplevel):
         self.tree.column("#4", width=60,  anchor="center")
         self.tree.column("#5", width=80,  anchor="center")
         self.tree.column("#6", width=150, anchor="center")
-        self.tree.column("#7", width=80,  anchor="center")
-        self.tree.column("#8", width=80,  anchor="center")
 
         paned_window.add(self.tree)
-        self.display_show_all_room()
+        self.display_show_all_keycard()
 
-    def display_show_all_room(self):
+    def display_show_all_keycard(self):
         # Connect to the SQLite database
         conn = sqlite3.connect(Oasis_database_full_path)
         cursor = conn.cursor()
 
         try:
-            if self.filter_var.get() == "Free rooms":
+            if self.filter_var.get() == "Free keycard":
+                print("HI")
                 # Select only free rooms
-                cursor.execute("""
-                    SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
-                    '', '', '', ''
-                    FROM Apartment_Info_TBL
-                    WHERE Apartment_Info_TBL.RoomID NOT IN (SELECT RoomID FROM Contract_TBL WHERE Status = 'Active')
-                    ORDER BY Apartment_Info_TBL.RoomID
-                """)
+                # cursor.execute("""
+                #     SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
+                #     '', '', '', ''
+                #     FROM Apartment_Info_TBL
+                #     WHERE Apartment_Info_TBL.RoomID NOT IN (SELECT RoomID FROM Contract_TBL WHERE Status = 'Active')
+                #     ORDER BY Apartment_Info_TBL.RoomID
+                # """)
 
             elif self.filter_var.get() == "Rent rooms":
+                print("HI")
                 # Select only rent rooms
-                cursor.execute("""
-                    SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
-                    CASE WHEN Contract_TBL.Status = 'Active' THEN Customer_TBL.FirstName || ' ' || Customer_TBL.LastName ELSE '' END AS Rent,
-                    COALESCE(Contract_TBL.StartDate, ''), COALESCE(Contract_TBL.EndDate, '')
-                    FROM Apartment_Info_TBL
-                    LEFT JOIN Contract_TBL ON Apartment_Info_TBL.RoomID = Contract_TBL.RoomID
-                    LEFT JOIN Customer_TBL ON Contract_TBL.CustomerID = Customer_TBL.CustomerID
-                    WHERE Contract_TBL.Status = 'Active'
-                    ORDER BY Apartment_Info_TBL.RoomID
-                """)
+                # cursor.execute("""
+                #     SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
+                #     CASE WHEN Contract_TBL.Status = 'Active' THEN Customer_TBL.FirstName || ' ' || Customer_TBL.LastName ELSE '' END AS Rent,
+                #     COALESCE(Contract_TBL.StartDate, ''), COALESCE(Contract_TBL.EndDate, '')
+                #     FROM Apartment_Info_TBL
+                #     LEFT JOIN Contract_TBL ON Apartment_Info_TBL.RoomID = Contract_TBL.RoomID
+                #     LEFT JOIN Customer_TBL ON Contract_TBL.CustomerID = Customer_TBL.CustomerID
+                #     WHERE Contract_TBL.Status = 'Active'
+                #     ORDER BY Apartment_Info_TBL.RoomID
+                # """)
 
             elif self.filter_var.get() == "Rent rooms order by customer":
+                print("HI")
                 # Select only rent rooms order by customer name
-                cursor.execute("""
-                    SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
-                    CASE WHEN Contract_TBL.Status = 'Active' THEN Customer_TBL.FirstName || ' ' || Customer_TBL.LastName ELSE '' END AS Rent,
-                    COALESCE(Contract_TBL.StartDate, ''), COALESCE(Contract_TBL.EndDate, '')
-                    FROM Apartment_Info_TBL
-                    LEFT JOIN Contract_TBL ON Apartment_Info_TBL.RoomID = Contract_TBL.RoomID
-                    LEFT JOIN Customer_TBL ON Contract_TBL.CustomerID = Customer_TBL.CustomerID
-                    WHERE Contract_TBL.Status = 'Active'
-                    ORDER BY Customer_TBL.FirstName
-                """)
+                # cursor.execute("""
+                #     SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
+                #     CASE WHEN Contract_TBL.Status = 'Active' THEN Customer_TBL.FirstName || ' ' || Customer_TBL.LastName ELSE '' END AS Rent,
+                #     COALESCE(Contract_TBL.StartDate, ''), COALESCE(Contract_TBL.EndDate, '')
+                #     FROM Apartment_Info_TBL
+                #     LEFT JOIN Contract_TBL ON Apartment_Info_TBL.RoomID = Contract_TBL.RoomID
+                #     LEFT JOIN Customer_TBL ON Contract_TBL.CustomerID = Customer_TBL.CustomerID
+                #     WHERE Contract_TBL.Status = 'Active'
+                #     ORDER BY Customer_TBL.FirstName
+                # """)
 
             else:
                 # Show all rooms
+                print("YES")
                 cursor.execute("""
-                    SELECT Apartment_Info_TBL.RoomID, Apartment_Info_TBL.RoomNo, Apartment_Info_TBL.Building, Apartment_Info_TBL.Floor, Apartment_Info_TBL.RoomType,
-                    CASE WHEN Contract_TBL.Status = 'Active' THEN Customer_TBL.FirstName || ' ' || Customer_TBL.LastName ELSE '' END AS Rent,
-                    COALESCE(Contract_TBL.StartDate, ''), COALESCE(Contract_TBL.EndDate, '')
-                    FROM Apartment_Info_TBL
-                    LEFT JOIN Contract_TBL ON Apartment_Info_TBL.RoomID = Contract_TBL.RoomID
-                    LEFT JOIN Customer_TBL ON Contract_TBL.CustomerID = Customer_TBL.CustomerID
-                    ORDER BY Apartment_Info_TBL.RoomID
+                    SELECT 
+                        Access_Card_Manage_TBL.ID, 
+                        COALESCE(Access_Card_Manage_TBL.KeycardID, '-') AS KeycardID,
+                        COALESCE(Customer_TBL.FirstName || ' ' || Customer_TBL.LastName, '-') AS CustomerName,
+                        COALESCE(Apartment_Info_TBL.RoomNo, '-') AS RoomNo,
+                        COALESCE(Employee_TBL.FirstName || ' ' || Employee_TBL.LastName, '-') AS EmployeeName,
+                        CASE 
+                            WHEN Access_Card_Manage_TBL.Status = 'Idle' THEN 'ว่าง'
+                            WHEN Access_Card_Manage_TBL.Status = 'Used' THEN 'ใช้งานอยู่'
+                            ELSE '-'
+                        END AS Status  
+                    FROM Access_Card_Manage_TBL
+                    LEFT JOIN Customer_TBL ON Access_Card_Manage_TBL.CustomerID = Customer_TBL.CustomerID
+                    LEFT JOIN Contract_TBL ON Customer_TBL.CustomerID = Contract_TBL.CustomerID
+                    LEFT JOIN Apartment_Info_TBL ON Contract_TBL.RoomID = Apartment_Info_TBL.RoomID
+                    LEFT JOIN Employee_TBL ON Access_Card_Manage_TBL.StaffID = Employee_TBL.EmployeeID
+                    ORDER BY Access_Card_Manage_TBL.ID;
                 """)
 
             rows = cursor.fetchall()
@@ -142,7 +152,7 @@ class Show_keycard_Page(tk.Toplevel):
 
     def apply_filter(self):
         self.tree.delete(*self.tree.get_children())
-        self.display_show_all_room()
+        self.display_show_all_keycard()
 
     def sort_treeview(self, col, reverse):
         data = [(self.tree.set(child, col), child) for child in self.tree.get_children('')]
